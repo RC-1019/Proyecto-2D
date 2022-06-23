d96ecd45f98da247bc8e9047d92cf7b9ec0af92d
RC-1019 <racantillo69@misena.edu.co>
2022-06-23 16:03:05 -0500

    Día 7

--- Proyecto 2D/Assets/Scripts/Limite.cs
+++ Proyecto 2D/Assets/Scripts/Limite.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 using UnityEngine.UI;
 public class Limite : MonoBehaviour
 {
+    public int puntaje = 100;
+    public int puntajeTotal;
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if(collision.tag == "Enemy")
+        {
+            Destroy(collision.gameObject);
+        }
+        else if (collision.tag == "Ally")
         {
             Destroy(collision.gameObject);
         }
--- Proyecto 2D/Assets/Scripts/Movimiento.cs
+++ Proyecto 2D/Assets/Scripts/Movimiento.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Enemies : MonoBehaviour
+public class Movimiento : MonoBehaviour
 {
     public float velocidadMovimiento;
     public Transform[] puntosMovimiento;
