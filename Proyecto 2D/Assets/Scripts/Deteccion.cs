d9a066ddfe94aa29c3ac4b659a3925b82a11fcd0
unknown <SENA@MEDAPRCCSFSD338.FormacionSena.RED>
2022-06-30 16:12:49 -0500

    Día 11
    Configuración de sonidos, animaciones y menús.

--- Proyecto 2D/Assets/Scripts/Deteccion.cs
+++ Proyecto 2D/Assets/Scripts/Deteccion.cs
@@ -7,27 +7,21 @@ public class Deteccion : MonoBehaviour
     [Header("Datos puntaje")]
     public int Score = 0;
     public int vidaNemesis = 2;
-    public GameObject[] sprites;
 
     [Header("Información sonido")]
     public AudioSource Source;
     public AudioClip[] efectos;
 
+    [Header("Datos de animación")]
+    public Animator animacion;
+
     private void OnMouseDown()
     {
-        GetComponent<CapsuleCollider2D>().enabled = false;
-        sprites[0].SetActive(false);
-        sprites[1].SetActive(true);
-        if (sprites[1]==true)
-        {
-            Destroy(gameObject, 0.6f);
-        }
-
-
-
+        
         if (tag == "Enemy")
         {
             AudioSource.PlayClipAtPoint(efectos[1], gameObject.transform.position);
+            animacion.SetBool("Muerte", true);
             Destroy(gameObject, 0.2f);
             ScoreUI.score += Score;            
         }
@@ -35,20 +29,23 @@ public class Deteccion : MonoBehaviour
         if (tag == "Jill")
         {
             AudioSource.PlayClipAtPoint(efectos[0], gameObject.transform.position);
+            animacion.SetBool("Muerte", true);
             Destroy(gameObject, 0.2f);
         }
 
         if (tag == "Carlos")
         {
             AudioSource.PlayClipAtPoint(efectos[0], gameObject.transform.position);
+            animacion.SetBool("Muerte", true);
+            Destroy(gameObject, 0.2f);
         }
 
         if (tag == "Nemesis")
         {
            AudioSource.PlayClipAtPoint(efectos[2], gameObject.transform.position);
+           animacion.SetBool("Muerte", true);
            Destroy(gameObject, 0.2f);
            ScoreUI.score += Score;
-           
         }
     }
     
